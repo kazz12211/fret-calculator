6a793d7ccb213240fffd0c2be88ee10f73ce9498
Kazuo Tsubaki <kazz12211@gmail.com>
2017-03-13 03:13:45 +0900

    Add translation (en and ja)

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -1,23 +1,49 @@
-var app = angular.module("app", []);
+var app = angular.module("app", [ 'pascalprecht.translate' ]);
 
 app.config(function($locationProvider) {
 	$locationProvider.html5Mode({
-		enabled:true,
-		requireBase:false
-	});
+		enabled : true,
+		requireBase : false
+	})
 });
+
+app.config(function($translateProvider) {
+	$translateProvider.useStaticFilesLoader({
+		prefix : 'strings/',
+		suffix : '.json'
+	});
+	$translateProvider.preferredLanguage(findBrowserLanguage());
+});
+
+function findBrowserLanguage() {
+	try {
+		return (navigator.browserLanguage || navigator.language || navigator.userLanguage)
+				.substr(0, 2)
+	} catch (e) {
+		return "en";
+	}
+}
 
 app.factory("$req", function($http, $location) {
 	var url = $location.absUrl();
 	return {
 		calcFretPositions : function(scale, numFrets) {
-			return $http.get(url + "calcFretPositions?scale="+scale+"&numFrets="+numFrets);
+			return $http.get(url + "calcFretPositions?scale=" + scale
+					+ "&numFrets=" + numFrets);
 		},
-		calcBridgePosition: function(scale, jointPos, neckAngle, stringHeight, fretHeight, thickness) {
-			return $http.get(url + "calcBridgePosition?scale="+scale+"&jointPos="+jointPos+"&neckAngle="+neckAngle+"&stringHeight="+stringHeight+"&fretHeight="+fretHeight+"&thickness="+thickness);
+		calcBridgePosition : function(scale, jointPos, neckAngle, stringHeight,
+				fretHeight, thickness) {
+			return $http.get(url + "calcBridgePosition?scale=" + scale
+					+ "&jointPos=" + jointPos + "&neckAngle=" + neckAngle
+					+ "&stringHeight=" + stringHeight + "&fretHeight="
+					+ fretHeight + "&thickness=" + thickness);
 		},
-		calcFingerboardSize : function(scale, numStrings, numFrets, nutPitch, saddlePitch, nutSpacing) {
-			return $http.get(url + "calcFingerboardSize?scale="+scale+"&numStrings="+numStrings+"&numFrets="+numFrets+"&nutPitch="+nutPitch+"&saddlePitch="+saddlePitch+"&nutSpacing="+nutSpacing);
+		calcFingerboardSize : function(scale, numStrings, numFrets, nutPitch,
+				saddlePitch, nutSpacing) {
+			return $http.get(url + "calcFingerboardSize?scale=" + scale
+					+ "&numStrings=" + numStrings + "&numFrets=" + numFrets
+					+ "&nutPitch=" + nutPitch + "&saddlePitch=" + saddlePitch
+					+ "&nutSpacing=" + nutSpacing);
 		}
 	}
 });
